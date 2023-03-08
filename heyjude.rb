98e0b9a642fd2b8650e0230dbd38a7644ae5119e
Stéfani Santini Reis <110628639+stefanisantini@users.noreply.github.com>
2023-03-07 21:52:56 -0300

    Add files via upload

---
+++ heyjude.rb
@@ -0,0 +1,117 @@
+puts "Hey Jude, don't make it bad."
+puts "Take a sad song and make it better."
+puts "Remember to let her under your skin,"
+puts "Then you'll begin to make it"
+
+6.times do
+    puts "Better"
+end
+puts "OH!"
+
+3.times do
+    puts "NA NA NA"    
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
+
+3.times do
+    puts "NA NA NA"
+end
+
+puts "Hey Jude..."
